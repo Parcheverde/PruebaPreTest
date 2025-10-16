b89fadd60d4ed7353768e803cacef16ba6369ead
ParcheVerde <guevararamirez121@gmail.com>
2025-10-16 13:03:29 -0600

    PlantillaVS

---
+++ CrudEF/CrudEF/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CrudEF
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CrudEF/CrudEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudEF.MvcApplication" Language="C#" %>
